cf343dc0b38d98f3c2bdfd89a76367383b6c3c65
Nessrine004 <Nessrine.Bekri@emsi-edu.ma>
2025-04-17 14:06:13 +0200

    V3

--- src/main/java/ma/enset/hopital/entities/Patient.java
+++ src/main/java/ma/enset/hopital/entities/Patient.java
@@ -19,4 +19,5 @@ public class Patient {
     private Date dateNaissance;
     private boolean malade;
     private int score;
+
 }
--- src/main/java/ma/enset/hopital/repository/PatientRepository.java
+++ src/main/java/ma/enset/hopital/repository/PatientRepository.java
@@ -1,7 +1,14 @@
 package ma.enset.hopital.repository;
 
 import ma.enset.hopital.entities.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNomContains(String keyword, Pageable pageable);
+
+    @Query("select p from Patient p where p.nom like :x")
+    Page<Patient> chercher(String keyword, Pageable pageable);
 }
